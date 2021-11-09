53d942027e624caa8bc208f3acbdab51fc689e97
João Victor <jovi47profissional@gmail.com>
2021-11-09 11:19:24 -0300

    Conectando ao mongo em Cloud

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/mostrese';
+const url = "mongodb+srv://dev:hXa2X23mQvg9Y4x6@cluster0.kppfn.mongodb.net/mostrese?retryWrites=true&w=majority";
 mongoose.connect(url, {useNewUrlParser: true});
 
 module.exports = mongoose;
