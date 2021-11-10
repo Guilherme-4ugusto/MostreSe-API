673a1723d585ed530b1762928d1e329ef2b7c2cc
João Victor <jovi47profissional@gmail.com>
2021-11-10 09:10:04 -0300

    Enviroment values já segurados na aplicação

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
 
-const url = "mongodb+srv://dev:hXa2X23mQvg9Y4x6@cluster0.kppfn.mongodb.net/mostrese?retryWrites=true&w=majority";
-mongoose.connect(url, {useNewUrlParser: true});
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
 
 module.exports = mongoose;
